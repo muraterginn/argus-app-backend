ed94a58f757fefd6974160e4a50275957c4741ff
Mehmet Murat Ergin <erginmm94@gmail.com>
2024-07-29 17:49:02 +0300

    Comment lines edited.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,13 +7,11 @@ import (
 )
 
 func main() {
-	// Konfigürasyonu yükle
 	cfg, err := config.LoadConfig(".")
 	if err != nil {
 		log.Fatalf("Could not load config: %v", err)
 	}
 
-	// TLS sunucusunu başlat
 	err = server.StartTLSServer(cfg)
 	if err != nil {
 		log.Fatalf("Failed to start TLS server: %v", err)
